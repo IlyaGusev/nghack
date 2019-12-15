ed2d84b7df3be035ab1b1395b4adc0edb578a8da
ddale <ddale@yandex-team.ru>
2019-12-15 12:24:21 +0300

    change callcenter path

--- solution/callcenter.py
+++ solution/callcenter.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     df_test = pd.read_csv(input_csv, index_col='id')
 
     model = catboost.CatBoostClassifier()
-    model.load_model('../models/call_center_catboost.model')
+    model.load_model('models/call_center_catboost.model')
 
     x_test = extract_features(df_test)
     prediction = (model.predict_proba(x_test)[:, 1] > 0.42).astype(int)
