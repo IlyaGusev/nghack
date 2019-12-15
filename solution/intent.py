1b572076b6ddafe1661309ce9aeab6c9fbcb9f74
Vadim Lyubanenko <lubanenko@yandex.ru>
2019-12-15 12:07:09 +0300

    intent.py baseline fasttext

--- solution/intent.py
+++ solution/intent.py
@@ -1,11 +1,28 @@
-import sys
+import argparse
+import fasttext
 import pandas
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input.csv')
+    parser.add_argument('output', help='output.csv')
+    parser.add_argument('--labels', help='fastText labels', required=True)
+    parser.add_argument('--model', help='fastText model', required=True)
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    input_csv = sys.argv[1]
-    output_csv = sys.argv[2]
-    
-    df_test = pandas.read_csv(input_csv, index_col='id')
-    df_test['label'] = 'мобильная связь - тарифы' # the most popular label
-    df_test.to_csv(output_csv)
+    args = parse_args()
+
+    to_label = pandas.read_csv(args.labels).to_dict('records')
+    to_label = {row['fasttext_label']:row['label'] for row in to_label}
+
+    df_test = pandas.read_csv(args.input, index_col='id')
+
+    model = fasttext.load_model(args.model)
+    preds = model.predict(list(df_test.text.values))
+
+    df_test['label'] = list(map(lambda x: to_label[x[0]], preds[0]))
+    df_test.to_csv(args.output)
